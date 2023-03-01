d5e14448e9f483d80c278a86bb6448bf37fd258f
Joel Conley <joel.conley@athennian.com>
2023-02-28 20:04:01 -0700

    FavoritesList cleaned and documented

--- my-app/src/components/FavoritesList.js
+++ my-app/src/components/FavoritesList.js
@@ -7,21 +7,18 @@ const FavoritesList = (props) => {
     const originalMovies = JSON.parse(localStorage.getItem("allMovies"));
     const [showHiX, setShowHiX] = useState(false);
     const [posterBeingRemoved, setPosterBeingRemoved] = useState(null);
-    const [posterBeingAdded, setPosterBeingAdded] = useState(null);
-    //console.log("faves",props.faves)
+
+    // get all the movies that are in the favorites list
     const favMovies = props.faves.map((fav) => {
-        //console.log("fav",typeof fav)
-        //console.log(props.movies)
         return originalMovies.find((movie) => {
-            //console.log(movie)
             return movie.id == fav
         })
-        });
-    //console.log("faveMovies",favMovies)
+    });
     
+    // remove a movie from the favorites list
     const handleFaveRemove = (movieId) => {
-        console.log("clicked fave", movieId)
         setPosterBeingRemoved(movieId);
+        // delay removal to allow animation to finish
         setTimeout(() => {
             setPosterBeingRemoved(null);
             props.removeFav(movieId);
@@ -31,24 +28,20 @@ const FavoritesList = (props) => {
     const toggleFaves = () => {
         setShowFaves(!showFaves);
     }
+
+    // if the favorites list is open, show the list, otherwise show the heart button to open it
     if (showFaves) {
     return (
     <div className={`mt-8 mr-10 right-0 top-0 h-screen overflow-y-auto`} >
-      {/* <h2 onClick={props.toggleFaves}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 mx-auto hover:cursor-pointer text-green-500">
-            <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-        </svg> 
-      </h2> */}
       <ul>
         {favMovies.map((movie) => {
             return (
                 <li key={movie.id} className="mt-4 mr-5" onMouseEnter={()=>setShowHiX(true)} onMouseLeave={()=>setShowHiX(false)}>
                     <Link to={`/movie/${movie.id}`}>
                         <img src={`https://image.tmdb.org/t/p/w92/${movie.poster}`} 
-                        title={movie.title} alt={movie.title} 
-                        className={`max-h-24 transition-all duration-1000 ${posterBeingRemoved==movie.id ? 'h-0 opacity-0' : 'h-24 opacity-100'}`}/>
+                            title={movie.title} alt={movie.title} 
+                            className={`max-h-24 transition-all duration-1000 ${posterBeingRemoved==movie.id ? 'h-0 opacity-0' : 'h-24 opacity-100'}`}/>
                     </Link>
-                    {/* <span>{movie.title} ({movie.release_date.slice(0, 4)})</span> */}
                     <div className={`relative left-12 bottom-24 w-fit p-1 cursor-pointer hover:bg-red-500 rounded z-1 -mb-6 ${showHiX ? 'opacity-100' : 'opacity-0'}`}>
                         <HiX size={12} className="text-white bg-red-500" onClick={() => handleFaveRemove(movie.id)} />
                     </div>
