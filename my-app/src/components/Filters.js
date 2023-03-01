1faf4e718a5103afe860aff37f25d357235235c4
Joel Conley <joel.conley@athennian.com>
2023-02-28 20:25:22 -0700

    Filters cleaned and documented

--- my-app/src/components/Filters.js
+++ my-app/src/components/Filters.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import SearchForm from './SearchForm';
 import YearForm from './YearForm';
 import SelectForm from'./SelectForm';
 import RatingForm from './RatingForm';
@@ -11,39 +10,30 @@ const Filters = (props) => {
     const [selectedFilter,setSelectedFilter]=React.useState('title');
     const [filtersVisible,setFiltersVisible]=React.useState(true);
   
+    //reset movie data to original state
     const removeAllFilters=()=>{
-        console.log("YOUR REMOVING ALL FILTERS ")
         props.resetData();
     }
     
-    const toggleFilters=()=>{
-        setFiltersVisible(!filtersVisible);
-        
-    }
-
     return ( 
         <aside className={`w-full px-2 bg-black bg-opacity-40 rounded-md transition-all duration-500 ease-in-out ${filtersVisible ? 'opacity-100' : 'opacity-0 -translate-x-full'}`}>
             <div className="w-full p-4">
                 <div className="flex flex-col items-center">
-                    {/* <div className='flex justify-end'> */}
                         <div className={`relative left-24 bottom-4 w-fit p-1 cursor-pointer rounded z-1 -mb-6`}>
                             <HiX size={20} className="text-white hover:bg-red-500" onClick={props.toggleFilters}/>
                         </div>
-                    {/* </div> */}
                     <h1 className='text-gray-300 font-sans font-light text-4xl leading-10 tracking-widest pb-10 border-b-2 border-double border-gray-500'> 
                         Filters
                     </h1>
                     <label className="block mb-2 text-gray-400 mt-2">Filter:</label>
                     <select value={selectedFilter} onChange={(e)=>{setSelectedFilter(e.target.value)}}
                             className="px-3 py-2 mb-4 text-gray-900 bg-white border border-gray-400 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent">
-                        {/* <option value="title">Title</option> */}
                         <option value="title">Title</option>
                         <option value="genre">Genre</option>
                         <option value="year">Year</option>
                         <option value="rating">Rating</option>
                     </select>
                     {selectedFilter==="title"? <SearchTitleForm filterTitle={props.filterTitle} searchForMovieTitle={props.searchForMovieTitle}/> : null}
-                    {/* {selectedFilter==="title" ? <SearchForm searchForMovieTitle={props.searchForMovieTitle} /> : null} */}
                     {selectedFilter==="genre" ?  <SelectForm movies={props.movies} onGenreSelect={props.onGenreSelect}/> : null}
                     {selectedFilter==="year" ?  <YearForm movies={props.movies} filterYear={props.filterYear}/> : null}
                     {selectedFilter==="rating" ? <RatingForm filterRating={props.filterRating}/> : null}
