e303b1740bdf8a3ce0910eadf2251afa41f98f2c
Joel Conley <joel.conley@athennian.com>
2023-02-28 19:57:22 -0700

    App cleaned and documented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,27 +9,19 @@ import React, { useEffect, useState }  from "react";
 
 
 const App = () => {
-  const [ogMovies,setOgMovies]= useState([]); // set an og state thing 
+  const [ogMovies,setOgMovies]= useState([]);
   const [faves, setFaves] = useState(JSON.parse(localStorage.getItem("faves")) || []); 
-  const [searchValue, setSearchValue]=useState('');
-  const [gSortList, setGSortList]=useState([]);
   const [isFetching, setIsFetching] = useState(false);
-  const [isEmpty,setIsEmpty]=useState(false);  // is empty is true 
-
   
 
   useEffect( ()=>{
     if(ogMovies.length <= 0){
       // first retrieve from local storage 
       const temp = localStorage.getItem("allMovies");
-      // const temp = null
-      // if it there?
       if(temp){
         let mdata = JSON.parse(temp);
-        // mdata.sort((a,b)=>a.title.localeCompare(b.title));
         sortMovies(mdata, "title");
         setOgMovies(mdata);
-        // setIsEmpty(false); 
       }else{
         setIsFetching(true);
         // if we dont have data then we need to fetch it 
@@ -42,12 +34,9 @@ const App = () => {
           }, 3000)
           // save in local storage 
           localStorage.setItem("allMovies", JSON.stringify(data))
-          // data.sort((a,b)=>a.title.localeCompare(b.title));
           sortMovies(data, "title");
           // set to movie state 
           setOgMovies(data);
-          // setIsEmpty(false);
-          // setMovies(ogMovies); // jill added to test brain idk if its right spot
           
         })
         .catch(err => console.log(err))
@@ -56,14 +45,10 @@ const App = () => {
   },[]) // empty array means only run once {{{  THIS LINE IS THE HOLY GRAIL OF THIS APP   }}}
 
   const resetData=()=>{
-      // first retrieve from local storage 
       const temp = localStorage.getItem("allMovies");
         let mdata = JSON.parse(temp);
-        // mdata.sort((a,b)=>a.title.localeCompare(b.title));
         sortMovies(mdata, "title");
-        console.log("rsettinhg data with mdata: ", mdata)
         setOgMovies(mdata);
-        // setOgMovies(false); 
   }
   
   const sortMovies = (movies, sortType, reverse=false) => {    
@@ -98,27 +83,24 @@ const App = () => {
     setOgMovies([...movies])
   }
 
-
+// add or remove a movie from the faves array
 const favHandler = (movieId) => {
-  console.log("faves before change", faves)
   // check if the movie is already in the faves array
   const isFave = faves.includes(movieId);
   if(isFave){
     // if it is then remove it 
     const newFaves = faves.filter(id=>id!==movieId);
-    console.log("faves after remove", newFaves)
     localStorage.setItem("faves", JSON.stringify(newFaves));
     setFaves(newFaves);
   }else{
     // if it is not then add it 
     const newFaves = [...faves, movieId];
-    console.log("faves after add", newFaves)
     localStorage.setItem("faves", JSON.stringify(newFaves));
     setFaves(newFaves);
   }
 }
 
-
+// filter movies by genre
 const onGenreSelect = (selected)=>{
   const originalMovies = JSON.parse(localStorage.getItem("allMovies"));
 
@@ -129,54 +111,49 @@ const onGenreSelect = (selected)=>{
       return(false) // no movie genres array in the movie object 
     }
   })
+
   sortMovies(moviesFilteredbyGenre, "title");
   setOgMovies(moviesFilteredbyGenre)
 }
 
+// filter movies by year
 const filterYear= (compareOperator, year)=>{
-  console.log("they chose: ",compareOperator);
-  console.log("by the year:",year);
   const originalMovies = JSON.parse(localStorage.getItem("allMovies"));  // get the original Movies in an array 
+
   const moviesFilteredByYear= originalMovies.filter(movie=>{
     const releaseDateOfMovie= parseInt(movie.release_date.slice(0,4))
-     console.log("the releaseDate of movie above is: ",releaseDateOfMovie)
-     if(compareOperator == "After"){
+
+    if(compareOperator == "After"){
        if(releaseDateOfMovie > year){
-        console.log(movie);
-        console.log("The release date of movie is larger then the selected value")
         return(true);
        }else{
         return(false);
        }
      } else if(compareOperator == "Before"){ 
       if(releaseDateOfMovie < year){
-        console.log(movie);
         return(true);
        }else{
         return(false);
        }
      }else{
       if(releaseDateOfMovie === year){
-        console.log(movie);
         return(true);
       }else{
         return(false);
       }
      }
   })
-  console.log(moviesFilteredByYear);
+
   sortMovies(moviesFilteredByYear, "year");
   setOgMovies(moviesFilteredByYear);
 }
 
+// filter movies by rating
 const filterRating =(rangeArray)=>{
-  console.log("this is range array in app",rangeArray);
-  //if(rangeArray.length==0){alert("There is no movies that match your range!")}
   const [r1, r2]= rangeArray;
-  console.log("this is r1: ",r1);
-  console.log("this is r2:", r2);
   let biggestValue;
   let smallestValue;
+  // get the biggest and smallest value
   if(r1>r2){
     biggestValue=r1;
     smallestValue=r2;
@@ -184,27 +161,26 @@ const filterRating =(rangeArray)=>{
     biggestValue=r2;
     smallestValue=r1;
   }
-  console.log("bigger: ",biggestValue, "smaller: ",smallestValue);
-  // get all oroginal movies 
-  const originalMovies = JSON.parse(localStorage.getItem("allMovies"));  // get the original Movies in an array 
+  
+  // get all original movies 
+  const originalMovies = JSON.parse(localStorage.getItem("allMovies"));  
+  
   const filteredByRange=originalMovies.filter(movie=>{
-    //console.log(movie);
     if(smallestValue <= movie.ratings.average && movie.ratings.average <=biggestValue){
-      console.log(movie.ratings.average);
       return(true);
     }else{
-      console.log("the movies average",movie.ratings.average," should bigger then: ",smallestValue, "and smaller then: ",biggestValue )
       return(false);
     }
   })
-  console.log(filteredByRange);
+  
   sortMovies(filteredByRange, "rating");
   setOgMovies(filteredByRange);
-  
 }
-  
+ 
+// filter movies by title
   const filterTitle=(searchedTitle)=>{
     const originalMovies = JSON.parse(localStorage.getItem("allMovies"));
+    
     const searchTitleArray = originalMovies.filter(movie => {
       if((movie.title).toString().toLowerCase().includes(searchedTitle.toLowerCase())){
         return(true);
@@ -212,27 +188,28 @@ const filterRating =(rangeArray)=>{
         return(false);
       }
     })
-    console.log(searchTitleArray);
+    
     sortMovies(searchTitleArray, "title");
     setOgMovies(searchTitleArray);
   }
 
-
+// add a user rating to a movie
   const addUserRating = (movieId, rating) => {
     const originalMovies = JSON.parse(localStorage.getItem("allMovies"));
     const movieToRate = originalMovies.find(movie=>movie.id == movieId);
+    
     //update count and average if no user rating exists yet
     if (!movieToRate.ratings.userRating) {
       movieToRate.ratings.count++
       movieToRate.ratings.average = Math.round((movieToRate.ratings.average * (movieToRate.ratings.count - 1) + rating) / movieToRate.ratings.count)
-    } else { //update average if user rating exists
+    } else { //update average only if user rating exists
       movieToRate.ratings.average = Math.round(((movieToRate.ratings.average * movieToRate.ratings.count) - movieToRate.ratings.userRating + rating) / movieToRate.ratings.count)
-  
     }
+    
+    //set user rating
     movieToRate.ratings["userRating"] = rating;
-    console.log("movieToRate after rating: ", movieToRate);
-    console.log("originalMovies after rating: ", originalMovies);
     localStorage.setItem("allMovies", JSON.stringify(originalMovies));
+    
     sortMovies(originalMovies, "title")
     setOgMovies(originalMovies);
   } 
@@ -240,12 +217,10 @@ const filterRating =(rangeArray)=>{
 return (
     <div className="App">
       <Header className="App-header" resetData={resetData}  />
-      {/* <ToastContainer key="toast-container" position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover/> */}
       <Routes>
           <Route path="/" element={<MovieSearch filterTitle={filterTitle} resetData={resetData} isLoading={isFetching} setIsLoading={setIsFetching} />} />
           <Route path="movies" element={<MovieBrowser currentMovies={ogMovies} movies={ogMovies} sortMovies={sortMovies} filterTitle={filterTitle} filterYear={filterYear} resetData={resetData} favHandler={favHandler} faves={faves} onGenreSelect={onGenreSelect} filterRating={filterRating} />} />
           <Route path="movie/:movieId" element={<MovieDetails movies={ogMovies} faves={faves} favHandler={favHandler} addUserRating={addUserRating}/>} />
-          {/* <Route path="/:movieId" element={<MovieDetails ={ogMovies[1]} />} /> */}
       </Routes> 
     </div>
   );
