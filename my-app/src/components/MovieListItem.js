9c70bec7aafdd5958c160667e60ac04befd9df8b
Joel Conley <joel.conley@athennian.com>
2023-02-28 21:12:03 -0700

    MovieListItem cleaned and documented

--- my-app/src/components/MovieListItem.js
+++ my-app/src/components/MovieListItem.js
@@ -6,42 +6,47 @@ const MovieListItem = (props) => {
   const posterUrl = `https://image.tmdb.org/t/p/w92/${props.movie.poster}`
   const navigate = useNavigate();
 
+  //handle click of favorite heart
   const favClickHandler = (e) => {
+    //stop event from bubbling up to row click handler
     e.stopPropagation();
     props.favHandler(props.movie.id);
-    
   }
+
+  //handle click of movie row
   const rowClickHandler = () => {
-    console.log(props.movie.id)
+    //navigate to movie details page
     navigate(`/movie/${props.movie.id}`)
   }
 
+  //set placeholder image if poster image is not found
   const onImageError = (e) => {
     e.target.onerror = null
     e.target.src = 'https://via.placeholder.com/92x138?text=No+Image'
   }
 
   return (
-            <tr onClick={rowClickHandler} className="hover:cursor-pointer hover:bg-slate-200">
-              <td className="px-6 py-4 whitespace-nowrap bg-black bg-opacity-10">
-                <img src={posterUrl} alt={props.movie.title} title={props.movie.title} onError={onImageError} className="mx-auto"/>
-                </td>
-                <td className="px-6 py-4 whitespace-normal  max-w-lg">
-                {props.movie.title}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {props.movie.release_date.slice(0,4)}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {props.movie.ratings.average}
-              </td>
-              <td className="px-6 py-4">
-                {props.movie.ratings.popularity.toFixed()}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap" onClick={(e)=>favClickHandler(e)}>
-                <FavHeart isFav={props.faves.includes(props.movie.id)}/>
-              </td>
-            </tr>
+          <tr onClick={rowClickHandler} className="hover:cursor-pointer hover:bg-slate-200">
+            <td className="px-6 py-4 whitespace-nowrap bg-black bg-opacity-10">
+              <img src={posterUrl} alt={props.movie.title} title={props.movie.title} onError={onImageError} className="mx-auto"/>
+            </td>
+            <td className="px-6 py-4 whitespace-normal  max-w-lg">
+              {props.movie.title}
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              {props.movie.release_date.slice(0,4)}
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              {props.movie.ratings.average}
+            </td>
+            <td className="px-6 py-4">
+              {props.movie.ratings.popularity.toFixed()}
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap" onClick={(e)=>favClickHandler(e)}>
+              {/* //pass boolean prop to FavHeart component based on whether movie is in favorites array, which determines whether heart is filled or not */}
+              <FavHeart isFav={props.faves.includes(props.movie.id)}/>
+            </td>
+          </tr>
           
   )
 }
